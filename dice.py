6906265c7f39b76bbe37d13f982f1a877bc1b572
Komalpreet Kaur <randhawakomal243@gmail.com>
2026-06-28 16:18:16 -0400

    Initial commit: create dice program structure

---
+++ dice.py
@@ -0,0 +1,30 @@
+import random
+
+class Die:
+    def __init__(self, sides=6):
+        self.sides = sides
+
+    def roll_die(self):
+        print(random.randint(1, self.sides))
+
+
+# 6-sided die
+print("6-sided die rolls:")
+die1 = Die()
+
+for _ in range(10):
+    die1.roll_die()
+
+# 10-sided die
+print("\n10-sided die rolls:")
+die2 = Die(10)
+
+for _ in range(10):
+    die2.roll_die()
+
+# 20-sided die
+print("\n20-sided die rolls:")
+die3 = Die(20)
+
+for _ in range(10):
+    die3.roll_die()
